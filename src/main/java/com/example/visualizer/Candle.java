792b9b4bbd3deacc1beaa8a409f19303d76b5e41
Diego-Urdaneta <91572741+Diego-Urdaneta@users.noreply.github.com>
2022-08-06 16:02:58 -0400

    Update Candle.java

--- src/main/java/com/example/visualizer/Candle.java
+++ src/main/java/com/example/visualizer/Candle.java
@@ -5,58 +5,75 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Candle extends Group{
+    double riser;
     Line YAxis;
     Line XAxis;
-    double YIter;
     double XIter;
+    Series series;
+    List<Data> rev_series;
 
-    Rectangle bear = new Rectangle();
-    Rectangle bull = new Rectangle();
-
-    Rectangle wick = new Rectangle();
-
-    public Candle(Series series, Axes axes, Data data){
+    public Candle(Series series, Axes axes) {
+        this.series = series;
+        riser = axes.getMover();
         XAxis = axes.getXAxis();
         YAxis = axes.getYAxis();
-        YIter = axes.getYIter();
-        XIter = axes.getXIter();
-        double YOffset = YAxis.getEndY()-10;
-        double YRatio = YAxis.getEndY()-YAxis.getStartY();
-        double XRatio = XAxis.getEndX()-XAxis.getStartX();
-        double SRatio = series.getHigh()-series.getLow();
+        XIter = series.series.size()+1;
 
-        bull.setWidth(YRatio/series.getSeries().size());
-        bull.setStroke(Color.WHITE);
-        bull.setFill(Color.GREEN);
-        bull.setStrokeWidth(bull.getWidth()*0.1);
-        bull.setHeight(YOffset-
-                ((YOffset- YAxis.getStartY())*(data.getOpen()-series.getLow())/SRatio) -
-                (YOffset-((data.getClose()-series.getLow())/SRatio)*(YOffset- YAxis.getStartY())));
-        bull.relocate(XAxis.getEndX() - (XRatio*(series.getSeries().indexOf(data)+1)/XIter) - bull.getWidth()/2 - bull.getStrokeWidth()/2,
-                (YOffset-((data.getClose()-series.getLow())/SRatio)* (YOffset- YAxis.getStartY())));
+    }
 
-        bear.setWidth(YRatio/series.getSeries().size());
-        bear.setStroke(Color.WHITE);
-        bear.setStrokeWidth(bear.getWidth()*0.1);
-        bear.setFill(Color.RED);
-        bear.setHeight(YOffset-
-                ((YOffset- YAxis.getStartY())*(data.getClose()-series.getLow())/SRatio) -
-                (YOffset-((data.getOpen()-series.getLow())/SRatio)*(YOffset - YAxis.getStartY())));
-        bear.relocate(XAxis.getEndX() - (XRatio*(series.getSeries().indexOf(data)+1)/XIter) - bear.getWidth()/2 - bear.getStrokeWidth()/2,
-                (YOffset-((data.getOpen()-series.getLow())/SRatio)* (YOffset - YAxis.getStartY())));
+    void clear(){
+        this.getChildren().clear();
+    }
 
-        wick.setWidth(bear.getWidth()*0.1);
-        wick.setFill(Color.WHITE);
-        wick.setHeight(YOffset-
-                ((YOffset-YAxis.getStartY())*(data.getLow()-series.getLow())/SRatio) -
-                (YOffset-((data.getHigh()-series.getLow())/SRatio)*(YOffset-YAxis.getStartY())));
-        wick.relocate(XAxis.getEndX() - (XRatio*(series.getSeries().indexOf(data)+1)/XIter) - wick.getWidth()/2,
-                (YOffset-((data.getHigh()-series.getLow())/SRatio)*(YOffset- YAxis.getStartY())));
+    void create(){
+        rev_series = series.series;
+        Collections.reverse(rev_series);
+        double YOffset = YAxis.getEndY() - 10;
+        double YRatio = YAxis.getEndY() - YAxis.getStartY();
+        double XRatio = XAxis.getEndX() - XAxis.getStartX();
+        double SRatio = series.getHigh() - series.getLow();
 
-        this.getChildren().add(wick);
-        this.getChildren().add(bear);
-        this.getChildren().add(bull);
+        for (Data data : rev_series) {
+            System.out.println(data.getVolume() +"  "+ data.getTime());
+            Rectangle bear = new Rectangle();
+            Rectangle bull = new Rectangle();
+            Rectangle wick = new Rectangle();
+
+            bull.setWidth(YRatio / series.series.size());
+            bull.setStroke(Color.WHITE);
+            bull.setFill(Color.GREEN);
+            bull.setStrokeWidth(bull.getWidth() * 0.1);
+            bull.setHeight(YOffset -
+                    ((YOffset - YAxis.getStartY()) * (data.getOpen() - series.getLow()) / SRatio) -
+                    (YOffset - ((data.getClose() - series.getLow()) / SRatio) * (YOffset - YAxis.getStartY())) + riser);
+            bull.relocate(XAxis.getEndX() - (XRatio * (series.series.indexOf(data) + 1) / XIter) - bull.getWidth() / 2 - bull.getStrokeWidth() / 2,
+                    (YOffset - ((data.getClose() - series.getLow()) / SRatio) * (YOffset - YAxis.getStartY())) + riser);
+
+            bear.setWidth(YRatio / series.series.size());
+            bear.setStroke(Color.WHITE);
+            bear.setStrokeWidth(bear.getWidth() * 0.1);
+            bear.setFill(Color.RED);
+            bear.setHeight(YOffset -
+                    ((YOffset - YAxis.getStartY()) * (data.getClose() - series.getLow()) / SRatio) -
+                    (YOffset - ((data.getOpen() - series.getLow()) / SRatio) * (YOffset - YAxis.getStartY())) + riser);
+            bear.relocate(XAxis.getEndX() - (XRatio * (series.series.indexOf(data) + 1) / XIter) - bear.getWidth() / 2 - bear.getStrokeWidth() / 2,
+                    (YOffset - ((data.getOpen() - series.getLow()) / SRatio) * (YOffset - YAxis.getStartY())) + riser);
+
+            wick.setWidth(bear.getWidth() * 0.1);
+            wick.setFill(Color.WHITE);
+            wick.setHeight(YOffset -
+                    ((YOffset - YAxis.getStartY()) * (data.getLow() - series.getLow()) / SRatio) -
+                    (YOffset - ((data.getHigh() - series.getLow()) / SRatio) * (YOffset - YAxis.getStartY())) + riser);
+            wick.relocate(XAxis.getEndX() - (XRatio * (series.series.indexOf(data) + 1) / XIter) - wick.getWidth() / 2,
+                    (YOffset - ((data.getHigh() - series.getLow()) / SRatio) * (YOffset - YAxis.getStartY())) + riser);
+
+            this.getChildren().add(wick);
+            this.getChildren().add(bear);
+            this.getChildren().add(bull);
+        }
     }
 }
-
