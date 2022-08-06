db634008d567192708d33e0a11b4b54be66c9427
Diego-Urdaneta <91572741+Diego-Urdaneta@users.noreply.github.com>
2022-08-06 16:08:19 -0400

    Update VolumeBars.java

--- src/main/java/com/example/visualizer/VolumeBars.java
+++ src/main/java/com/example/visualizer/VolumeBars.java
@@ -6,33 +6,41 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 public class VolumeBars extends Group {
-    Rectangle bar = new Rectangle();
     Line YAxis;
     Line XAxis;
     Line VYAxis;
     double XIter;
+    Series series;
 
-    public VolumeBars(Series series, Axes axes, Data data){
+    public VolumeBars(Series series, Axes axes){
         XAxis = axes.getXAxis();
         YAxis = axes.getYAxis();
         VYAxis = axes.getVYAxis();
-        XIter = axes.getXIter();
+        XIter = series.series.size()+1;
+        this.series = series;
+    }
 
+    void create(){
         double YRatio = YAxis.getEndY()-YAxis.getStartY();
         double XRatio = XAxis.getEndX()-XAxis.getStartX();
 
-        bar.setWidth(YRatio/series.getSeries().size());
-        bar.setStroke(Color.WHITE);
-        bar.setFill(Color.DEEPSKYBLUE);
-        bar.setStrokeWidth(bar.getWidth()*0.1);
-        bar.relocate(
-                XAxis.getEndX() - (XRatio*(series.getSeries().indexOf(data)+1)/XIter) - bar.getWidth()/2 - bar.getStrokeWidth()/2,
-                (VYAxis.getEndY()-10-((data.getVolume()/
-                        (series.getVHigh()))*(VYAxis.getEndY()-20-VYAxis.getStartY()))));
-        bar.setHeight(((data.getVolume()/
-                (series.getVHigh()))*(VYAxis.getEndY()-20-VYAxis.getStartY())));
+        for (Data data: series.series) {
+            Rectangle bar = new Rectangle();
 
-        this.getChildren().add(bar);
+            bar.setWidth(YRatio/series.series.size());
+            bar.setStroke(Color.WHITE);
+            bar.setFill(Color.DEEPSKYBLUE);
+            bar.setStrokeWidth(bar.getWidth()*0.1);
+            bar.relocate(
+                    XAxis.getEndX() - (XRatio*(series.series.indexOf(data)+1)/XIter) - bar.getWidth()/2 - bar.getStrokeWidth()/2,
+                    (VYAxis.getEndY()-10-((data.getVolume()/
+                            (series.getVHigh()))*(VYAxis.getEndY()-20-VYAxis.getStartY()))));
+            bar.setHeight(((data.getVolume()/
+                    (series.getVHigh()))*(VYAxis.getEndY()-20-VYAxis.getStartY())));
+
+            this.getChildren().add(bar);
+        }
     }
 
+    void clear(){this.getChildren().clear();}
 }
